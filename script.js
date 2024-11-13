239d9edca9c13517f339bc41330981a12744cc70
Alexander Mills <alexander.mills@gtri.gatech.edu>
2024-11-12 19:39:30 -0500

    created grid creation functions

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //Create a webpage with a 16x16 grid of square divs.
+const container = document.querySelector(".container");
+const btn = document.querySelector("button");
+
+function createGrid(gWidth, gHeight){
+    numCells = gWidth * gHeight;
+    for(let i=0; i<numCells; i++){
+        createCell();
+    }
+}
+
+function createCell(){
+    const cell = document.createElement("div");
+    cell.style.backgroundColor = "green";
+    cell.style.width = "10px";
+    cell.style.height = "10px";
+    container.appendChild(cell);
+}
+
+createGrid(16,16);
+
